63104d06a0d852d90fce4dc975c998c95f648178
VANIERCOLLEGE\2295472 <2295472@edu.vaniercollege.qc.ca>
2023-08-04 13:59:24 -0400

    all tests working, removed excess code from test

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -14,30 +14,13 @@ describe("HelloWorld.vue", () => {
 import CourseItem from "@/components/CourseItem.vue";
 
 it("renders props.course when passed", () => { 
-  const course = {
 
-        type: Object,
-      default() {
-        return {
-          id: 0,
-          name: "Course Name",
-          credits: 0,
-          hours: 0,
-          description:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis accusantium modi adipisci rem architecto sequi atque mollitia voluptates magnam assumenda at reiciendis aliquid, iusto ab debitis quibusdam molestiae quas commodi?",
-          location: "online",
-          enrollment: 0,
-        };
-
-    },
- 
-};
   const wrapper = shallowMount(CourseItem);
   expect(wrapper.find("[data-testid='course-name']").text()).toBe("Course Name");
 });
 
 
-it("shows the button when the course is not added", () => {
+it("shows the add course button when the course is not added", () => {
   const wrapper = shallowMount(CourseItem);
   const data = {  data() {
     return {
@@ -50,6 +33,71 @@ it("shows the button when the course is not added", () => {
   expect (wrapper.find("[data-testid='add']").text()).toBe(buttonText);
 
 });
+
+it("shows the add course button when the course is not full", () => {
+  const wrapper = shallowMount(CourseItem);
+ 
+  const buttonText = "Add Course";
+ 
+  expect (wrapper.find("[data-testid='add']").text()).toBe(buttonText);
+
+});
+
+//  it("hides the add course button when the course enrollment is full", async () => {
+//   const buttonText = "Add Course";
+//   const wrapper = shallowMount(CourseItem);
+//   const course = {
+//     type: Object,
+//     default() {
+//       return {
+//         enrollment: 20,
+//       };
+//     },
+//   };
+
+//   expect (wrapper.find("[data-testid='add']").exists()).toBe(false);
+// });
+  
+
+
+
+it("hides the remove course button when the course is not added", async () => {
+  const buttonText = "Remove Course";
+  const wrapper = shallowMount(CourseItem);
+//   const data = {  data() {
+//     return {
+//       isAdded: false,
+//     };
+//   },
+// };
+await wrapper.setData({ isAdded: false });
+expect (wrapper.find("[data-testid='remove']").exists()).toBe(false); 
+});
+
+// it("shows the remove course button when the course is added", async () => {
+//   const buttonText = "Remove Course";
+//   const wrapper = shallowMount(CourseItem);
+//   const method = {   methods: {
+//     sendAdd() {
+//       console.log(1);
+//       this.$emit("addCourse", this.course.id);
+//       console.log(3);
+//       this.isAdded = true;
+//     },
+//   },
+// };
+
+//   const data = {  data() {
+//   return {
+//     isAdded: true,
+//   };
+// },
+// };
+// expect (wrapper.find("[data-testid='remove']").exists()).toBe(true);
+//   await wrapper.setData({ isAdded: true });
+// });
+
+
 
 // it("emits an action when the button is clicked", async () => {
 //   const wrapper = shallowMount(CourseItem);
